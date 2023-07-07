30fc68c68125411357c8a4d4278b9d3429baed5f
seem <mwlorgat@gmail.com>
2023-07-07 14:20:24 +0200

    testing test parallelization

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,14 @@
 
 CODAL_CACHE_DIR=$(mktemp -d)
 
-CODAL_CACHE_DIR="$CODAL_CACHE_DIR" pytest "$@" &&
+CODAL_CACHE_DIR="$CODAL_CACHE_DIR" pytest "$@"
+
+# Re-enable if/when we get lots of parallelizable tests
+# CODAL_CACHE_DIR="$CODAL_CACHE_DIR" pytest -n auto -m "not serial" "$@" &&
+# CODAL_CACHE_DIR="$CODAL_CACHE_DIR" pytest -m "serial" "$@"
+
+EXIT_CODE=$?
 
 rm -rf "$CODAL_CACHE_DIR"
+
+exit $EXIT_CODE
